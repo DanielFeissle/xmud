7b3b056717de3b4b2db44649aa6a8bb041b66b67
Daniel Feissle <dfeissle@msn.com>
2021-06-10 20:27:17 -0500

    Fix display for banner script
    Cleaned up script to no longer use a temp file
    File can still be saved but serves no program use
    Updated some of the letters

--- pban.ps1
+++ pban.ps1
@@ -2,37 +2,38 @@
 #powershell banner
 #6-1-2021 v1
 #call this powershell .\pban <txt> <keepfile>
+#changes
+#6-10-2021 v1.1
+#Fix display for banner
+
 param([String]$ban="aaa", [int32]$ktxt="0") 
 #file to be kept is the temp file, 0 deletes and 1 keeps
 #$ban=$args[0]
 $pu=0
-Write-Output "|" "|" "|" "|" "|" "|" "|"> "$PSScriptRoot\temp.txt"
 $ban | ForEach-Object { $ay=$_.ToCharArray() }
-$ay  | ForEach-Object { 
-    #$du=$_
+for ($i = 0; $i -le 6; $i++) {
+	$pu=0
+	$ay  | ForEach-Object { 
     $h= $_
     If(!(test-path "$PSScriptRoot\dat\banner\$h.txt"))
     {
         Write-Warning "FILE NOT FOUND: $PSScriptRoot\dat\banner\$h.txt"
         Write-Warning "ATTEMPTED TEXT: $ban"
-
     }
     else {
-        $a=Get-Content "$PSScriptRoot\dat\banner\$h.txt"
-        $fileContent = Get-Content "$PSScriptRoot\temp.txt"
-        for ($i = 0; $i -le 6; $i++) {
-        
-            $fileContent[$i] += $a[$i]
-        
-        
-        }
-        $fileContent | Set-Content "$PSScriptRoot\temp.txt"
-
+		$a=Get-Content "$PSScriptRoot\dat\banner\$h.txt"
+		$test=$test+$a[$i]
+		$a=Get-Content "$PSScriptRoot\dat\banner\$h.txt"
+		if ($ban.Length-1 -eq $pu) {
+			#escape the single line here
+			$test=$test+"`n"
+		}
     }
 $pu=$pu+1
+	}
 }
-Get-Content "$PSScriptRoot\temp.txt"
-if ($ktxt -eq 0)
+Write-Output $test
+if ($ktxt -eq 1)
 {
-    Remove-Item "$PSScriptRoot\temp.txt"
+    Write-Output "$test" > "$PSScriptRoot\temp.txt"
 }
