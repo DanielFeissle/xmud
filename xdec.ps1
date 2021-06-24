3bba098a6d6397983a39e8efd0485f54c81cdd34
Daniel Feissle <dfeissle@msn.com>
2021-06-24 18:24:52 -0500

    Begin to standardize process flow
    Fix process staying after script execution
    Added data protections to prevent data loss

--- xdec.ps1
+++ xdec.ps1
@@ -1,7 +1,7 @@
 #Excel Encrypt (xenc)
 #June 14 2021
 #Usage: .\xdec <EXCEL File to be obstrusted> <COLUMN to be muddled>
-param([String]$xlsxInput="file.xlsx", [int32]$ktxt="0") 
+param([int32]$colval="4", [String]$xlsxInput="file.xlsx", [int32]$ktxt="0") 
 
 
 #hmm in powershell it is tougher to overwrite existing variables, It is applicable but not recommended 
@@ -52,74 +52,109 @@ Write-Warning   "TEST"
 #call banner script here
 & .\pban.ps1 "xdec"
 
-
- Get-Content "$PSScriptRoot\temp.txt"
-Remove-Item "$PSScriptRoot\out\tempFile_dec.xlsx"
-$xFile="$PSScriptRoot\out\tempFile_dec.xlsx"
- $ExcelWB = new-object -comobject excel.application
- Write-Output "Converting to xlsx"
- Get-ChildItem -Path "$PSScriptRoot\out" -Filter "*.xlsx" | ForEach-Object{
-     Write-Output "$_"
-     $Workbook = $ExcelWB.Workbooks.Open($_.Fullname) 
-     $worksheet = $Workbook.sheets.item("Sheet1")
-     $countUsed = $worksheet.UsedRange.Rows.Count
-     Write-Output "INTEROP counts this many rows: $countUsed"
-     $countColumns = $worksheet.UsedRange.Columns.Count
-     Write-Output "INTEROP counts this many columns: $countColumns"
-   # Write-Output "">key.txt
-     for ($i = 1; $i -le $countUsed; $i++) {
-         $uid=New-Guid
+###############
 
 
-         #if ( $ws.Cells.Item(4, $i).Value2 -eq "sort" ) {
-         #$tc= $worksheet.Cells.Item($i, 4).Value2 #get the tc#
-		 $tc = (Get-Content "$PSScriptRoot\key.txt")[$i]
-if ($tc -notmatch '........-....-....-....-............')
+# Get-Content "$PSScriptRoot\temp.txt"
+#Remove-Item "$PSScriptRoot\out\tempFile_dec.xlsx"
+#$xFile="$PSScriptRoot\out\tempFile_dec.xlsx"
+#####################
+$ExcelWB = new-object -comobject excel.application
+Write-Output "Converting to xlsx"
+$sel=Get-ChildItem -Path $PSScriptRoot -Filter "*.xlsx" 
+$sela=$sel.Name
+if (($null -eq $sela) -or !(test-path "$PSScriptRoot\keys\$sela.$colval.exf"))
 {
-	if ( $tc -ne $null)
-	{
-	   $uuid=$worksheet.Cells.Item($i, 4).Value2
-	   $worksheet.Cells.Item($i, 4).Value2="$tc"
-	}
+	Remove-Item ShutDownWatcher #remove the watch file
+	Write-Warning "No valid xlsx files found in directory: $pwd"
+	exit 1
+}
+else {
+Get-ChildItem -Path $PSScriptRoot -Filter "*.xlsx" | ForEach-Object{
+	Write-Output "$_"
+	$valName=$_
+	$Workbook = $ExcelWB.Workbooks.Open($_.Fullname) 
+	for ($i = 1; $i -le $Workbook.Sheets.Count; $i++)
+	   {
+		  $worksheet = $Workbook.Sheets.Item($i)
+			$countUsed = $worksheet.UsedRange.Rows.Count
+			Write-Output "INTEROP counts this many rows: $countUsed"
+			$countColumns = $worksheet.UsedRange.Columns.Count
+			Write-Output "INTEROP counts this many columns: $countColumns"
+		   #Write-Output "">key.txt
+		   Write-Output "Extract column for faster processing"
+		   $testCol=$WorkSheet.Columns($colval)
+		   $testV=($testCol[1].Value2 -split '\r?\n').Trim()
+		   #$ft=$testV | Group-Object -AsHashTable -AsString
+			
+			for ($i = 0; $i -le $countUsed; $i++) {
+				$uid=$testV[$i]
+				$tc = (Get-Content "$PSScriptRoot\keys\$sela.$colval.exf")[$i]
+				#$tc=$testV[$i]
+				#echo "THI " $tc
+			   # echo "THIS IS" $testV[$i]
+				if ( $tc -ne $null)
+				{
+					if ($tc -ne "")
+					{
+						if ($uid -ne "")
+						{
+						 #  Write-Output "$tc">>key.txt
+						   $testV[$i]="$tc"
+						   #$df=$ft["$tc"].Count
+						   $WorkSheet.Columns.Replace("$uid","$tc") | out-null
+						   $te = $te + 1
+						}
+					} 
+				
+				
+				}
+			
+			
+			
+			
+			   # echo "$i" "$tc"
+		   ##	Write-Host -NoNewline "."
+				}
+				#$testCol.Value2=$testV
+				Write-Host ""
+				Write-Host -NoNewline "Toss column back in,"
+				for ($t=1; $t -le $countUsed; $t++)
+				{
+				   $worksheet.Cells.Item($t, $colval).Value2=$testV[$t-1]
+				}
+				Write-Host -NoNewline " Done."
+				Write-Host ""
+	   #echo "FINAL RESULT" $testV "END"
+			#	 $WorkSheet.Columns(4)= $testCol.Value2
+	   }
 
+	$Workbook.SaveAs("$PSScriptRoot\$valName.temp")
+	#$Workbook.SaveAs("$xFile", 6) #6 is for xlsx
+	$Workbook.Close($false)
 
-
-
-   # echo "$i" "$tc"
-	}
-	else {
-		if ($tc -ne $null)
-		{
-			$uuid=$worksheet.Cells.Item($i, 4).Value2
-			$WorkSheet.Columns.Replace("$uuid","$tc") | out-null
-
-		}
-
-	}
 }
 
-     $Workbook.SaveAs("$xFile")
-     #$Workbook.SaveAs("$xFile", 6) #6 is for xlsx
-     $Workbook.Close($false)
- }
 
+$ExcelWB.quit()
+}
 
- $ExcelWB.quit()
-
-
- #clean exit, we don't want no excel processes building up and potentially crashing
- [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook)
- [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
- [GC]::Collect()
-
-
+#clean exit, we don't want no excel processes building up and potentially crashing
+[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($testCol)
+[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet)
+[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook)
+[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
+[GC]::Collect()
+Remove-Item "keys\\$valName.$colval.exf"
+Remove-Item "$PSScriptRoot\$valName"
+Move-Item -Path "$PSScriptRoot\$valName.temp" -Destination "$PSScriptRoot\$valName"
 ##wrap up and finish with excel file here
 #Write-Output ""
 #$wb.SaveAs($xlsxInput+"_new",6)
 #$wb.Close($false)
 # $ExcelWB.quit()
 #clean exit, we don't want no excel processes building up and potentially crashing
-[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet)
+
 #[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($wb)
 ###[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
 [GC]::Collect()
--- xenc.ps1
+++ xenc.ps1
@@ -1,7 +1,7 @@
 #Excel Encrypt (xenc)
 #May 27 2021
 #Usage: .\xenc <EXCEL File to be obstrusted> <COLUMN to be muddled>
-param([String]$xlsxInput="file.xlsx", [int32]$ktxt="0") 
+param([int32]$colval="4", [String]$xlsxInput="file.xlsx", [int32]$ktxt="0") 
 
 
 #hmm in powershell it is tougher to overwrite existing variables, It is applicable but not recommended 
@@ -45,6 +45,10 @@ If(!(test-path "$PSScriptRoot\out"))
 Write-Output $null >> ShutDownWatcher #create a file at the begining
 
 
+If(!(test-path "$PSScriptRoot\\keys"))
+{
+      New-Item -ItemType Directory -Force -Path "$PSScriptRoot\\keys"
+}
 
 
 Write-Warning   "TEST"
@@ -53,90 +57,124 @@ Write-Warning   "TEST"
 & .\pban.ps1 "xenc"
 
 
- Get-Content "$PSScriptRoot\temp.txt"
-Remove-Item "$PSScriptRoot\out\tempFile.xlsx"
-$xFile="$PSScriptRoot\out\tempFile.xlsx"
+
+
+#################
  $ExcelWB = new-object -comobject excel.application
  Write-Output "Converting to xlsx"
- Get-ChildItem -Path $PSScriptRoot -Filter "*.xlsx" | ForEach-Object{
-     Write-Output "$_"
-     $Workbook = $ExcelWB.Workbooks.Open($_.Fullname) 
-     $worksheet = $Workbook.sheets.item("Sheet1")
-     $countUsed = $worksheet.UsedRange.Rows.Count
-     Write-Output "INTEROP counts this many rows: $countUsed"
-     $countColumns = $worksheet.UsedRange.Columns.Count
-     Write-Output "INTEROP counts this many columns: $countColumns"
-    Write-Output "">key.txt
-	Write-Output "Extract column for faster processing"
-	$testCol=$WorkSheet.Columns(4)
-	$testV=($testCol[1].Value2 -split '\r?\n').Trim()
-     for ($i = 0; $i -le $countUsed; $i++) {
-         $uid=New-Guid
-		 $tc=$testV[$i]
-		# echo "THIS IS" $testV[$i]
-         if ( $tc -ne $null)
-         {
-			 if ($tc -ne "")
-			 {
-				Write-Output "$tc">>key.txt
-				$testV[$i]="$uid"
-				$WorkSheet.Columns.Replace("$tc","$uid") | out-null
-				$te = $te + 1
-			 } 
+ $sel=Get-ChildItem -Path $PSScriptRoot -Filter "*.xlsx" 
+ if ($null -eq $sel -or (test-path "$PSScriptRoot\keys\$sel.$colval.exf"))
+{
+	Remove-Item ShutDownWatcher #remove the watch file
+	Write-Warning "No valid xlsx files found in directory: $pwd"
+	Write-Warning "File may not be present or file may already be encrypted"
+	exit 1
+}
+else {
+	Get-ChildItem -Path $PSScriptRoot -Filter "*.xlsx" | ForEach-Object{
+		$valName=$_
+		If((test-path "$PSScriptRoot\\$valName.temp"))
+		   {
+			   Remove-Item "$PSScriptRoot\\$valName.temp"
+		   }
+   
+   
+	   $Workbook = $ExcelWB.Workbooks.Open($_.Fullname) 
+	   for ($i = 1; $i -le $Workbook.Sheets.Count; $i++)
+		  {
+			 $worksheet = $Workbook.Sheets.Item($i)
+			   $countUsed = $worksheet.UsedRange.Rows.Count
+			   Write-Output "INTEROP counts this many rows: $countUsed"
+			   $countColumns = $worksheet.UsedRange.Columns.Count
+			   Write-Output "INTEROP counts this many columns: $countColumns"
+			  Write-Output "">"keys\\$valName.$colval.exf"
+			  Remove-Item "keys\\$valName.$colval.exf"
+			  Write-Output "Extract column for faster processing"
+			  $testCol=$WorkSheet.Columns($colval)
+			  $testV=($testCol[1].Value2 -split '\r?\n').Trim()
+			  #$ft=$testV | Group-Object -AsHashTable -AsString
+			   
+			   for ($i = 0; $i -le $countUsed; $i++) {
+				   $uid=New-Guid
+				   $tc=$testV[$i]
+				   
+				  # echo "THIS IS" $testV[$i]
+				   if ( $tc -ne $null)
+				   {
+					   if ($tc -ne "")
+					   {
+						  Write-Output "$tc">>"keys\\$valName.$colval.exf"
+						  $testV[$i]="$uid"
+						  #$df=$ft["$tc"].Count
+						  $WorkSheet.Columns.Replace("$tc","$uid") | out-null
+						  $te = $te + 1
+					   } 
+				   
+				   
+				   }
+			   
+			   
+			   
+			   
+				  # echo "$i" "$tc"
+			  ##	Write-Host -NoNewline "."
+				   }
+				   #$testCol.Value2=$testV
+				   Write-Host ""
+				   Write-Host -NoNewline "Toss column back in,"
+				   for ($t=1; $t -le $countUsed; $t++)
+				   {
+					  $worksheet.Cells.Item($t, $colval).Value2=$testV[$t-1]
+				   }
+				   Write-Host -NoNewline " Done."
+				   Write-Host ""
+		  #echo "FINAL RESULT" $testV "END"
+			   #	 $WorkSheet.Columns(4)= $testCol.Value2
+		  }
+		$randHour=  Get-Random -Maximum 23
+		$randMin=Get-Random -Maximum 59
+		$randSec=Get-Random -Maximum 59
+		$yestDay=(get-date (get-date).addDays(-1) -UFormat "%m/%d/%y")
+		(Get-Item "keys\\$valName.$colval.exf").LastWriteTime = "$yestDay ${randHour}:${randMin}:${randSec}"
+	   $Workbook.SaveAs("$PSScriptRoot\$valName.temp")
+	   #$Workbook.SaveAs("$xFile", 6) #6 is for xlsx
+	   $Workbook.Close($false)
+   
+   
+   
+   
+	}
+	$ExcelWB.quit()
 
 
-         }
+	#clean exit, we don't want no excel processes building up and potentially crashing
+	[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($testCol)
+	[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet)
+	[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook)
+	[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
+	[GC]::Collect()
+
+	Remove-Item "$PSScriptRoot\$valName"
+	Move-Item -Path "$PSScriptRoot\$valName.temp" -Destination "$PSScriptRoot\$valName"
+   ##wrap up and finish with excel file here
+   #Write-Output ""
+   #$wb.SaveAs($xlsxInput+"_new",6)
+   #$wb.Close($false)
+   # $ExcelWB.quit()
+   #clean exit, we don't want no excel processes building up and potentially crashing
+
+   #[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($wb)
+   ###[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
+   [GC]::Collect()
+   
+   Remove-Item ShutDownWatcher #remove the watch file
+   #Remove-Item $destinationExcel\\*.xlsx
+   #Rename-Item -Path ($xlsxInput+"_new") -NewName "$xlsxInput"
+   
+   
+   Write-Output "xlsx file created"
+   exit 0
+}
 
 
 
-
-        # echo "$i" "$tc"
-		Write-Host -NoNewline "."
-         }
-		 #$testCol.Value2=$testV
-		 Write-Host ""
-		 Write-Host -NoNewline "Toss column back in,"
-		 for ($t=1; $t -le $countUsed; $t++)
-		 {
-			$worksheet.Cells.Item($t, 4).Value2=$testV[$t-1]
-		 }
-		 Write-Host -NoNewline " Done."
-		 Write-Host ""
-#echo "FINAL RESULT" $testV "END"
-	 #	 $WorkSheet.Columns(4)= $testCol.Value2
-
-
-     $Workbook.SaveAs("$xFile")
-     #$Workbook.SaveAs("$xFile", 6) #6 is for xlsx
-     $Workbook.Close($false)
-
- }
-
-
- $ExcelWB.quit()
-
-
- #clean exit, we don't want no excel processes building up and potentially crashing
- [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook)
- [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
- [GC]::Collect()
-
-
-##wrap up and finish with excel file here
-#Write-Output ""
-#$wb.SaveAs($xlsxInput+"_new",6)
-#$wb.Close($false)
-# $ExcelWB.quit()
-#clean exit, we don't want no excel processes building up and potentially crashing
-[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet)
-#[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($wb)
-###[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelWB)
-[GC]::Collect()
-
-Remove-Item ShutDownWatcher #remove the watch file
-#Remove-Item $destinationExcel\\*.xlsx
-#Rename-Item -Path ($xlsxInput+"_new") -NewName "$xlsxInput"
-
-
-Write-Output "xlsx file created"
-exit 0
